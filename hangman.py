91a020a47010ff93d2a719d229590eada04f3d86
Wangdj-python <fcdjwang@mail.ustc.edu.cn>
2022-04-27 21:55:09 +0800

    my first? commit

---
+++ hangman.py
@@ -0,0 +1,51 @@
+# http://tinyurl.com/jhrvs94
+import random
+def hangman(word):
+    wrong = 0
+    stages = [" ",
+              "______    ",
+              "|         ",
+              "|     |   ",
+              "|     o   ",
+              "|    /|\  ",
+              "|    / \  ",
+              "|         ",
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome  to  Hangman ")
+
+# http://tinyurl.com/ztr5jc
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+            print((" ".join(board)))
+            e = wrong + 1
+            print("\n".join(stages[0: e]))
+        if "__" not in board:
+            print("You Win")
+            print((" ".jion(board)))
+            win = True
+            break
+
+    if not win:
+        print("\n".join(stages[0:wrong]))
+        print("You lose! It was {}.".format(word))
+
+
+
+c = ["Cat","Tiger","Dog","apple","Elephant","Dragen"]
+
+e = len(c)-1
+f = random.randint(0,e)
+g = c[f]
+print(g)
+hangman(g)
